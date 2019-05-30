32a258fdd7796ad98cb243fea58876bd99535112
b0bland <alfredychan96@gmail.com>
2019-05-29 23:52:49 -0700

    Added pseudocode

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,44 @@
 $(document).ready(function() {
 
-    var playerptcount;
-    var targetpt;
-    var randval1;
-    var randval2;
-    var randval3;
-    var randval4;
+    //Initial numbers are generated, and points start at zero//
+    var playerptcount = 0;
+    var targetpt = Math.floor(Math.random() * 50);
+    var randval1 = Math.floor(Math.random() * 5);
+    var randval2 = Math.floor(Math.random() * 10);
+    var randval3 = Math.floor(Math.random() * 20);
+    var randval4 = Math.floor(Math.random() * (-5));
 
-    $(".char").one("click", function() {
-        
-        var randval = Math.floor(Math.random() * 5)
-    });
     
 
-    // $("#char1").on("click", function() {
-    //     var randval = Math.floor(Math.random() * 5)
-    //     playerptcount = playerptcount + randval;
-    //     $("#score").html(playerptcount);
-    //     console.log(randval)
-    // })
+    console.log(targetpt, randval1, randval4)
+    
+    
+    //On click, the functions should grab the current point count, and then add it to the randomly generated value. and then push that value back to the current point count//
+
+    $("#char1").on("click", function() {
+        var current = playerptcount;
+        var newnum = current + randval1;
+        playerptcount.append(newnum);
+    })
+
+    $("#char2").on("click", function() {
+        var current = playerptcount;
+        var newnum = current + randval2;
+        playerptcount.append(newnum);
+    })
+
+    $("#char3").on("click", function() {
+        var current = playerptcount;
+        playerptcount = current + randval3;
+    })
+
+    $("#char4").on("click", function() {
+        var current = playerptcount;
+        playerptcount = current + randval4;
+    })
+
+    console.log(playerptcount)
+
+    //Once current point count is equal to the target point count, a win is added, points are reset, and numbers are generated again//
+    //If current point count exceeds target point count, a loss is added, points are reset, and numbers are generated again//
 })
