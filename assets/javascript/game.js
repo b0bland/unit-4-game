f61ab4525469ebda4fc23fc2c2ebcca278d10115
b0bland <alfredychan96@gmail.com>
2019-05-29 23:17:31 -0700

    Added initial variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,22 @@
 $(document).ready(function() {
 
+    var playerptcount;
+    var targetpt;
+    var randval1;
+    var randval2;
+    var randval3;
+    var randval4;
+
+    $(".char").one("click", function() {
+        
+        var randval = Math.floor(Math.random() * 5)
+    });
     
+
+    // $("#char1").on("click", function() {
+    //     var randval = Math.floor(Math.random() * 5)
+    //     playerptcount = playerptcount + randval;
+    //     $("#score").html(playerptcount);
+    //     console.log(randval)
+    // })
 })
