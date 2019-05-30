18766c18f234f012ee90e15facb4eb9377e56671
b0bland <alfredychan96@gmail.com>
2019-05-30 02:51:22 -0700

    Added readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # unit-4-game
+
+This is a game where the player clicks on four different boxes. Each box has a different value, the goal is to reach the target value without going over. Three of the buttons make the user score go up, while the far right one makes the user score go down. 
+
+Work is still being done on the game, including stylistic work and fixing some code. 
+
+##Technologies Used
+* HTML
+* CSS
+* Javascript
+* jQuery
+
+##Authors
+* Alfred Chan
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,9 +10,16 @@ $(document).ready(function() {
 
     var playerptcount = 0;
 
+    var win = 0;
+    var loss = 0;
+
     console.log(targetpt, randval1, randval4)
     
-    
+    $("#target").html("Target score: " + targetpt);
+    $("#score").html(playerptcount);
+    $("#win").html("Wins: " + win);
+    $("#loss").html("Losses: " + loss);
+
     //On click, the functions should grab the current point count, and then add it to the randomly generated value. and then push that value back to the current point count//
 
     $("#char1").on("click", function() {
@@ -39,8 +46,30 @@ $(document).ready(function() {
         $("#score").html(playerptcount);
     })
 
-    console.log(playerptcount)
 
     //Once current point count is equal to the target point count, a win is added, points are reset, and numbers are generated again//
     //If current point count exceeds target point count, a loss is added, points are reset, and numbers are generated again//
+
+    if (playerptcount === targetpt) {
+        win++;
+        targetpt = Math.floor(Math.random() * 37) + 13;
+        randval1 = Math.floor(Math.random() * 5) + 1;
+        randval2 = Math.floor(Math.random() * 10) + 1;
+        randval3 = Math.floor(Math.random() * 20) + 1;
+        randval4 = Math.floor(Math.random() * (-5));
+        playerptcount = 0;
+        $("#win").html("Wins: " + win);
+        alert("You win!");
+    }
+    else if (playerptcount > targetpt) {
+        loss++;
+        targetpt = Math.floor(Math.random() * 37) + 13;
+        randval1 = Math.floor(Math.random() * 5) + 1;
+        randval2 = Math.floor(Math.random() * 10) + 1;
+        randval3 = Math.floor(Math.random() * 20) + 1;
+        randval4 = Math.floor(Math.random() * (-5));
+        playerptcount = 0;
+        $("#loss").html("Losses: " + loss);
+        alert("You lose!");
+    }
 })
